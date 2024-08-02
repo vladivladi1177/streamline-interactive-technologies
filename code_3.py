cce8303c49628368cf1d19221ea45a8cbead84e1
vladivladi1177 <161205114+vladivladi1177@users.noreply.github.com>
2024-08-02 05:21:58 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,45 @@
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    return data;
+    });
+
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = generateRandomData();
+function generateRandomData() {
+}
+    }
+
+
+}
+main();        int[] data = new int[10];
+public class RandomDataGenerator {
+}
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+            System.out.println("Random Number: " + item);
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        }
+        for (int item : data) {    for (let i = 0; i < 10; i++) {
+function main() {
+    }
+    return data;
+        console.log("Random Number: " + item);
+function generateRandomData() {
+
+
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+    });
+main();
+}
+    data.forEach(item => {
+
+}
